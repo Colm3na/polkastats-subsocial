f959235b4ded15e5a1a6de09cd4e82e057bb2436
mariopino <mariopino@protonmail.com>
2021-08-06 11:06:43 +0200

    remove post link

--- frontend/components/extrinsics/posts/CreatePost.vue
+++ frontend/components/extrinsics/posts/CreatePost.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="text-left">
     <div v-if="activityOnly">
-      Created
-      <a :href="`https://app.subsocial.network/post/${postId}`" target="_blank"
+      Created post
+      <!-- <a :href="`https://app.subsocial.network/post/${postId}`" target="_blank"
         >post #{{ args[0] }}</a
-      >
+      > -->
       with
       <a
-        :href="`https://ipfs.io/api/v0/dag/get?arg=${args[2].ipfs}`"
+        :href="`https://ipfs.io/api/v0/dag/get?arg=${
+          args[2].ipfs ? args[2].ipfs : args[2].IPFS
+        }`"
         target="_blank"
         >content</a
       >
     </div>
     <div v-else>
-      Created
-      <a :href="`https://app.subsocial.network/post/${postId}`" target="_blank"
+      Created post
+      <!-- <a :href="`https://app.subsocial.network/post/${postId}`" target="_blank"
         >post #{{ args[0] }}</a
-      >
+      > -->
       with content:<br />
       <pre class="mb-0">{{ JSON.stringify(content, null, 2) }}</pre>
     </div>
@@ -49,7 +51,11 @@ export default {
   },
   async created() {
     await axios
-      .get(`https://ipfs.io/api/v0/dag/get?arg=${this.args[2].ipfs}`)
+      .get(
+        `https://ipfs.io/api/v0/dag/get?arg=${
+          this.args[2].ipfs ? this.args[2].ipfs : this.args[2].IPFS
+        }`
+      )
       .then(({ data }) => {
         this.content = data
       })
