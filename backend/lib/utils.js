7bdf2f9028c71719fc041c49ec4f0771564273d8
mariopino <mariopino@protonmail.com>
2021-12-02 20:20:36 +0100

    fix extrinsic.meta.docs

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -86,7 +86,7 @@ module.exports = {
       const { method } = extrinsic.toHuman().method;
       const args = JSON.stringify(extrinsic.args);
       const hash = extrinsic.hash.toHex();
-      const doc = extrinsic.meta.doc.toString().replace(/'/g, "''");
+      const doc = extrinsic.meta.docs.toString().replace(/'/g, "''");
       const success = module.exports.getExtrinsicSuccess(index, blockEvents);
       const sql = `INSERT INTO extrinsic (
           block_number,
